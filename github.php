fab678419dce53ca71318c23973cd30571da2071
sanjay <sanjayletuscode@gmail.com>
2023-11-30 12:47:13 +0530

    Addon a new files

---
+++ github.php
@@ -0,0 +1,19 @@
+Starting with Basic Git Commands on Ubuntu
+The following is a list of useful Git commands to help you get started:
+
+Find changed files in the working directory: git status
+Change to tracked files: git diff
+Add all changes to your next commit: git add
+Add selected changes into your next commit: git add -p
+Change the last commit: git commit -amend
+Commit all local changes in tracked files: git commit -a
+Commit previously staged changes: git commit
+Rename a Local branch git branch -m new-name
+List all currently configured remotes: git remote -v
+View information about a remote: git remote show
+Add a new remote repository: git remote add
+Delete a remote repository git remote remove [remote name]
+Download all changes from a remote repository: git fetch
+Download all changes from and merge into HEAD: git pull branch
+Create a new branch with the command: git branch first-branch
+To see more Git commands use: git --help
