a787abddfe101abbfbe290e33a30ecad79ec0f1e
sanjay <sanjayletuscode@gmail.com>
2023-11-30 13:10:56 +0530

    Addon a new files

--- github.php
+++ github.php
@@ -17,3 +17,18 @@ Download all changes from a remote repository: git fetch
 Download all changes from and merge into HEAD: git pull branch
 Create a new branch with the command: git branch first-branch
 To see more Git commands use: git --help
+
+…or create a new repository on the command line
+echo "# git-demo" >> README.md
+  git init
+  git add README.md
+  git commit -m "first commit"
+  git branch -M main
+  git remote add origin https://github.com/developersksingh/git-demo.git
+  git push -u origin main
+…or push an existing repository from the command line
+git remote add origin https://github.com/developersksingh/git-demo.git
+  git branch -M main
+  git push -u origin main
+…or import code from another repository
+You can initialize this repository with code from a Subversion, Mercurial, or TFS project.
